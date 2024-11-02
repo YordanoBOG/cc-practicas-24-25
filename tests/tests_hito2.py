b702c1b95a84b2554904ed037825c83a1653ad47
YordanoBOG <5802bo@gmail.com>
2024-11-02 19:53:07 +0100

    Hito 2, tests con print definidos para comprobar que se ejecutan correctamente

--- tests/tests_hito2.py
+++ tests/tests_hito2.py
@@ -1,5 +1,5 @@
 # assert statements: Each test uses an assert statement to check that the result of add()
 # matches the expected output. If the assertion fails, pytest will raise an error, marking
 # the test as failed.
-
-print ("tests del hito 2 ejecutados")
+def test_hito_2():
+    print ("tests del hito 2 ejecutados")
