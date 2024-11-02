eeabeb7df2d879362d089b97dda0ec8c5d803757
YordanoBOG <5802bo@gmail.com>
2024-11-02 19:56:11 +0100

    Hito 2, tests con print definidos para comprobar que se ejecutan correctamente

--- tests/tests_hito2.py
+++ tests/tests_hito2.py
@@ -2,4 +2,4 @@
 # matches the expected output. If the assertion fails, pytest will raise an error, marking
 # the test as failed.
 def test_hito_2():
-    print ("tests del hito 2 ejecutados")
+    assert True
