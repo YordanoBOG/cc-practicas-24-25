a9d89513537edc5a4e767fa36f1b4a6e97284549
YordanoBOG <5802bo@gmail.com>
2024-11-28 16:09:46 +0100

    Creacion de ficheros del hito 3. Primera prueba exitosa de API

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 # Import routes (important: import after creating `app` to avoid circular imports)
-from app.api import crearworkflow
+from app.api import crearworkflow # Se debe poner la ruta absoluta del entorno de ejecución, que parte del fichero raíz
