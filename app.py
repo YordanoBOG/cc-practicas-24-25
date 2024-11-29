884c3b05eec168faf72035aa3fc0ab4a0e31a17c
YordanoBOG <5802bo@gmail.com>
2024-11-29 01:44:44 +0100

    Creacion de ficheros del hito 3. Desarrollando API para crear workflows. ver-2

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+import logging
+
 from app import app  # Import the Flask app from the `app/app.py` file
 
+# Configuración de logging
+logging.basicConfig(level=logging.INFO,  # Nivel mínimo para registrar (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+                    format='%(asctime)s - %(levelname)s - %(message)s',  # Formato del log
+                    handlers=[
+                        logging.FileHandler("app.log"),  # Guardar logs en un archivo
+                        logging.StreamHandler()          # Mostrar logs en la consola
+                    ])
+
 if __name__ == "__main__":
+    app.logger.info("Lanzando la API de GeneSys")
     app.run(debug=True, host='0.0.0.0', port=8000)
--- app/api.py
+++ app/api.py
@@ -1,12 +1,22 @@
+import logging
+import json
 from flask import Flask, request, jsonify
 from app import app  # Import the app instance from `app/__init__.py`
 
-from modules.PATRIC_protein_processing.isolate_column import IsolateColumn 
+from modules.PATRIC_protein_processing.isolate_column import IsolateColumn
 from modules.PATRIC_protein_processing.generate_fasta import GenerateFasta
 from modules.PATRIC_protein_processing.reduce_sample import ReduceSample
 from modules.PATRIC_protein_processing.get_30kb_upanddown import Get30KbProteins
 from modules.PATRIC_protein_processing.get_codons_from_features import GetCodonsFromFeatures
 from modules.baseobjects import Workflow
+
+# Configuración de logging
+logging.basicConfig(level=logging.INFO,  # Nivel mínimo para registrar (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+                    format='%(asctime)s - %(levelname)s - %(message)s',  # Formato del log
+                    handlers=[
+                        logging.FileHandler("app.log"),  # Guardar logs en un archivo
+                        logging.StreamHandler()          # Mostrar logs en la consola
+                    ])
 
 ###############################################################################
 ###############################################################################
@@ -14,6 +24,7 @@ from modules.baseobjects import Workflow
 
 @app.route('/crearworkflow', methods=['GET'])
 def crear_workflow():
+    app.logger.info("Llamada a /crearworkflow")
     new_workflow = Workflow()
     new_workflow_parameters = new_workflow.get_parameters()
     return jsonify({"tareas": new_workflow_parameters['tasks'],
@@ -26,7 +37,8 @@ def crear_workflow():
 
 @app.route('/crearworkflowparametros', methods=['POST'])
 def crear_workflow_con_parametros():
-    parametros = request.json
+    parametros = json.loads(request.json)
+    app.logger.info(f"Llamada a /crearworkflowparametros con parametros:{str(parametros)}")
 
     # Comprobar si los parámetros incluyen una lista de tareas, un valor de salida y un fichero de resultados
     if "tasks" not in parametros:
