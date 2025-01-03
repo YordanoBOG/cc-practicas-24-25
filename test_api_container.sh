a5205ae29b17d9e2b84b4e2e491ef37f8c27a2f4
YordanoBOG <5802bo@gmail.com>
2025-01-03 11:47:29 +0100

    Contenerización de tareas: IsolateColumn, GenerateFasta, ReduceSample, test.yml

---
+++ test_api_container.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sleep 300;
+
+# Operaciones con la base de datos
+curl -X DELETE http://localhost:8000/delete/BVBRC_slatt_protein_small.csv;
+curl -X DELETE http://localhost:8000/delete/BVBRC_slatt_protein_small_new.csv;
+curl -X GET http://localhost:8000/consultdb;
+curl -F "file=@./BVBRC_slatt_protein_small.csv" http://localhost:8000/upload;
+curl -X GET http://localhost:8000/consultdb;
+
+# Operaciones con la API
+curl -X GET http://localhost:8000/crearworkflow;
+curl -X GET http://localhost:8000/showinfo;
+curl -X POST http://localhost:8000/crearworkflowparametros -H "Content-Type: application/json" \
+-d '{"tasks": [], "results_file": "workflow_res.txt", "returned_value": 0, "containerized": true}';
+curl -X POST http://localhost:8000/aniadirtareaisolatecolumn -H "Content-Type: application/json" \
+-d '{"containerized": true, "csv_path": "BVBRC_slatt_protein_small.csv", "col_name": "BRC ID"}';
+
+# Logger
+curl -X POST -d '{"message": "test"}' http://localhost:24224/test.tag
